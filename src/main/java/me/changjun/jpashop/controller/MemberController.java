0a1633cde3d056edfda236935a4dcca69f4f15e2
changjun89 <leechang0423@naver.com>
2019-11-17 21:06:55 +0900

    MemberController

---
+++ src/main/java/me/changjun/jpashop/controller/MemberController.java
@@ -0,0 +1,41 @@
+package me.changjun.jpashop.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.changjun.jpashop.domain.Address;
+import me.changjun.jpashop.domain.Member;
+import me.changjun.jpashop.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(@Valid MemberForm form, BindingResult bindResult) {
+        if (bindResult.hasErrors()) {
+            return "members/createMemberForm";
+        }
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setUsername(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+}
