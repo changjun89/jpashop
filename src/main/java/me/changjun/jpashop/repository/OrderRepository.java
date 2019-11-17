cfd2a9c024ae0ea8eb091bd40121415e15c0612c
changjun89 <leechang0423@naver.com>
2019-11-17 16:40:44 +0900

    OrderRepository

---
+++ src/main/java/me/changjun/jpashop/repository/OrderRepository.java
@@ -0,0 +1,22 @@
+package me.changjun.jpashop.repository;
+
+import lombok.RequiredArgsConstructor;
+import me.changjun.jpashop.domain.Order;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+}
