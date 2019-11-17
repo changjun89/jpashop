844ae65b890c12c05ed4a55f2fc25f0a7f1b3a1d
changjun89 <leechang0423@naver.com>
2019-11-18 00:38:40 +0900

    Member restApi update

--- src/main/java/me/changjun/jpashop/api/MemberApiController.java
+++ src/main/java/me/changjun/jpashop/api/MemberApiController.java
@@ -1,12 +1,11 @@
 package me.changjun.jpashop.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import me.changjun.jpashop.domain.Member;
 import me.changjun.jpashop.service.MemberService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -32,6 +31,14 @@ public class MemberApiController {
         return new CreateMemberResponse(memberId);
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id
+            , @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member updateMember = memberService.findOne(id);
+        return new UpdateMemberResponse(updateMember.getId(), updateMember.getUsername());
+    }
+
     @Data
     static class CreateMemberResponse {
         private Long id;
@@ -44,6 +51,19 @@ public class MemberApiController {
     @Data
     static class CreateMemberRequest {
         @NotEmpty
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        @NotEmpty
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
         private String name;
     }
 }
--- src/main/java/me/changjun/jpashop/service/MemberService.java
+++ src/main/java/me/changjun/jpashop/service/MemberService.java
@@ -44,4 +44,10 @@ public class MemberService {
     public Member findOne(Long id) {
         return memberRepository.find(id);
     }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.find(id);
+        member.setUsername(name);
+    }
 }
