f85594dd6d08986e990d8ca9651d924cfddc1a38
leechang0423 <leechang0423@naver.com>
2019-11-19 13:11:25 +0900

    OrderSimpleApiController V1.2 hibernate5Module add

--- src/main/java/me/changjun/jpashop/JpashopApplication.java
+++ src/main/java/me/changjun/jpashop/JpashopApplication.java
@@ -1,5 +1,6 @@
 package me.changjun.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,5 +10,10 @@ public class JpashopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpashopApplication.class, args);
+    }
+
+    @Bean
+    public Hibernate5Module hibernate5Module() {
+        return new Hibernate5Module();
     }
 }
